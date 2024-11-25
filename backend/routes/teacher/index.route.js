6447f00cf1ef961be6ec0f763ab403f3626c90e3
Alien <minhhnghiii@gmail.com>
2024-11-25 16:04:45 +0700

    hoàn thành câu hỏi nghe, điền khuyết bên giáo viên

--- backend/routes/teacher/index.route.js
+++ backend/routes/teacher/index.route.js
@@ -1,6 +1,7 @@
 import questionManagementRoute from "./questionManagement.route.js";
 import examManagementRoute from "./examManagement.route.js";
 import questionTypeManagementRoute from "./questionTypeManagement.route.js";
+
 const indexTeacher = (app) => {
   app.use("/teacher", questionManagementRoute);
   app.use("/teacher/exam", examManagementRoute);
