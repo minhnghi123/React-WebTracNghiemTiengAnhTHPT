4cc332500a874eec1383d703b5cc0826931a630a
Alien <minhhnghiii@gmail.com>
2025-03-22 21:12:09 +0700

    update

--- backend/routes/client/index.route.js
+++ backend/routes/client/index.route.js
@@ -4,12 +4,10 @@ import questionRoute from "./question.route.js";
 import examRoute from "./exam.route.js";
 import resultRoute from "./result.route.js";
 import flashCardRoute from "./flashcard.route.js";
-<<<<<<< HEAD
+
 import listeningExamRoute from "./listening-exams.route.js";
-=======
-import listeningExamRoute from "./listening-exams.js";
 import Classroom from "./classroom.route.js";
->>>>>>> eb8748e40074c60161288176a42f60ed8a969926
+
 import { protectedRoute } from "../../middlewares/protectedRoute.middleware.js";
 
 const indexClient = (app) => {
@@ -22,6 +20,5 @@ const indexClient = (app) => {
   app.use("/listening-exam", listeningExamRoute);
   app.use("/classroom", Classroom);
   app.use("/", homeRoute);
-
 };
 export default indexClient;
